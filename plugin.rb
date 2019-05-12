9412d106c46e47745d691de509ec8615561fd7af
marguerite <i@marguerite.su>
2019-05-12 23:34:59 +0800

    fix

--- plugin.rb
+++ plugin.rb
@@ -5,13 +5,18 @@
 # url: https://github.com/openSUSE-zh/discourse-openbuildservice-onebox
 
 gem 'watir','6.16.5'
-gem 'selenium-webdriver','~> 3.6'
+gem 'regexp_parser','1.3.0'
+gem 'selenium-webdriver','3.141.0'
+gem 'childprocess','0.6.3'
+gem 'ffi','1.10.0'
+gem 'rubyzip','1.2.2'
 
 enabled_site_setting :open_build_service_instance
 
 register_asset 'stylesheets/openbuildservice.scss'
 
 require 'uri'
+require 'watir'
 require_relative 'engine/openbuildservice_onebox'
 
 Onebox.options.load_paths.push(File.join(File.dirname(__FILE__), "templates"))
