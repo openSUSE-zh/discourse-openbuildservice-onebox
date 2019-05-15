010e877929e90403443f399523dc60832c5d1b9c
marguerite <i@marguerite.su>
2019-05-15 12:15:08 +0800

    fix scss

--- plugin.rb
+++ plugin.rb
@@ -4,10 +4,16 @@
 # authors: Marguerite Su <marguerite@opensuse.org>
 # url: https://github.com/openSUSE-zh/discourse-openbuildservice-onebox
 
+gem 'regexp_parser','1.3.0'
+gem 'ffi','1.10.0'
+gem 'childprocess', '0.6.3'
+gem 'rubyzip', '1.2.2'
+gem 'selenium-webdriver', '3.141.0'
+gem 'watir', '6.16.5'
+
+require 'uri'
 require 'watir'
 require_relative 'engine/openbuildservice_onebox'
-
-enabled_site_setting :open_build_service_instance
 
 register_asset 'stylesheets/openbuildservice.scss'
 
